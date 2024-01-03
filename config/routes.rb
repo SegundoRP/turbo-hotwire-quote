4c8386f1bf101bc040cce7df0b4e32661f8b1514
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-01-02 23:05:10 -0500

    feat: :sparkles: update controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'line_item_dates/new'
+  get 'line_item_dates/create'
   get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 
   root to: "pages#home"
 end
