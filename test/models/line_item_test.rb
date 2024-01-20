3ea7549705135cb3206402e8bb5c11be3bf4645a
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-01-20 02:39:51 -0500

    test: :white_check_mark: add test for update de total amount

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -4,4 +4,20 @@ class LineItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "#total_price returns the total price of the line item" do
+    assert_equal 250, line_items(:catering_today).total_price
+  end
+
+  test "Creating a new line item" do
+    assert_text number_to_currency(@quote.total_price)
+  end
+
+  test "Updating a line item" do
+    assert_text number_to_currency(@quote.total_price)
+  end
+
+  test "Destroying a line item" do
+    assert_text number_to_currency(@quote.total_price)
+  end
 end
--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,11 +1,14 @@
 require "application_system_test_case"
 
 class LineItemDatesTest < ApplicationSystemTestCase
+  include ActionView::Helpers::NumberHelper
+
   setup do
     login_as users(:accountant)
 
     @quote          = quotes(:first)
     @line_item_date = line_item_dates(:today)
+    @line_item      = line_items(:room_today)
 
     visit quote_path(@quote)
   end
@@ -46,5 +49,33 @@ class LineItemDatesTest < ApplicationSystemTestCase
     end
 
     assert_no_text I18n.l(Date.current, format: :long)
+  end
+
+  test "Updating a line item date" do
+    assert_selector "h1", text: "First quote"
+
+    within id: dom_id(@line_item_date, :edit) do
+      click_on "Edit"
+    end
+
+    assert_selector "h1", text: "First quote"
+
+    fill_in "Date", with: Date.current + 1.day
+    click_on "Update date"
+
+    assert_text I18n.l(Date.current + 1.day, format: :long)
+  end
+
+  test "Destroying a line item date" do
+    assert_text I18n.l(Date.current, format: :long)
+
+    accept_confirm do
+      within id: dom_id(@line_item_date, :edit) do
+        click_on "Delete"
+      end
+    end
+
+    assert_no_text I18n.l(Date.current, format: :long)
+    assert_text number_to_currency(@quote.total_price)
   end
 end
